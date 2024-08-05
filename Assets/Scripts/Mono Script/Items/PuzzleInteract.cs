b6f30c5dfeccda755c32e11e7b5507f4ad34083f
Rubick07 <evanjonatham07@gmail.com>
2024-08-05 20:52:06 +0700

    ObjectiveManager + PipePuzzle

--- Assets/Scripts/Mono Script/Items/PuzzleInteract.cs
+++ Assets/Scripts/Mono Script/Items/PuzzleInteract.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using RDCT.PlayerController;
 
 public class PuzzleInteract : InteractObject
 {
+    public CinemachineVirtualCamera virtualCamera;
+    private PuzzleBase puzzle;
+    public bool minigameskelar = false;
+    private void Start()
+    {
+        puzzle = GetComponent<PuzzleBase>();
+    }
 
     public override void Interaction()
     {
-        Debug.Log("Ini Puzzle");
+        if (minigameskelar)
+        {
+            return;
+        }
+        virtualCamera.gameObject.SetActive(true);
+        FindObjectOfType<InputManager>().GetComponent<InputManager>().enabled = false;
+        puzzle.enabled = true;
+
     }
 
 }
---
+++ Assets/Scripts/Mono Script/Puzzle/ObjectiveManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    private int PipePoint;
+
+    public void PipeObjectiveClear()
+    {
+        PipePoint++;
+    }
+
+
+}
