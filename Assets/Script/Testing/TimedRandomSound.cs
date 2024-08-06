26eee466db28efe4c184e5fe8b070942cff9437f
WhiteStyx <luasshiro@gmail.com>
2024-08-06 17:44:51 +0700

    nothing

---
+++ Assets/Script/Testing/TimedRandomSound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedRandomSound : MonoBehaviour
+{
+    float[] time;
+    
+    void Start()
+    {
+        int randomNumber = Random.Range(0,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
