ba9f8ee26c456c6f5a2174d8925d71bf58d0251d
Rird <r944514@gmail.com>
2024-08-10 14:08:24 +0700

    Dialog Improvement

--- Assets/Scripts/Mono Script/Dialog/dialogBase.cs
+++ Assets/Scripts/Mono Script/Dialog/dialogBase.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class dialogBase : MonoBehaviour
 {
@@ -18,27 +19,36 @@ public class dialogBase : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        textBox.gameObject.SetActive(false);
+    }
+
     public void panggilDialog(string titleDial)
     {
         dialog titledialogs = Array.Find<dialog>(dialogs, s => s.title == titleDial);
 
         if (titledialogs != null)
         {
+            textBox.gameObject.SetActive (true);
+
             if (titledialogs.speaker.Length > 0)
                 titledialogs.dialogGUI.text = titledialogs.speaker + " : " + titledialogs.dialogs;
             else
                 titledialogs.dialogGUI.text = titledialogs.dialogs;
-            StartCoroutine(waktuTunggu(titledialogs, titledialogs.dialogs.Length / 3));
+
+            StartCoroutine(waktuTunggu(titledialogs, titledialogs.dialogs.Length / 10, titledialogs));
         }
         else return;
     }
 
-    IEnumerator waktuTunggu(dialog ini, float tungguLama)
+    IEnumerator waktuTunggu(dialog ini, float tungguLama, dialog dialo)
     {
         while (true)
         {
             yield return new WaitForSeconds(tungguLama);
             hentikanDialog(ini);
+            dialo.dialogGUI.gameObject.SetActive(false);
             break;
         }
     }
