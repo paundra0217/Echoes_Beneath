689a921015ef7592b3e6fcf48e99d0eaaa9acc1e
Richz <r944514@gmail.com>
2024-10-02 13:13:46 +0700

    Bisa Muterin Generator

--- Assets/Scripts/Mono Script/Puzzle/Lever.cs
+++ Assets/Scripts/Mono Script/Puzzle/Lever.cs
@@ -1,32 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
     public bool Kunci = false;
     [SerializeField] Transform PusatRotasi;
+    //[SerializeField] Transform ParTrans;
     [SerializeField] float PosisiAtas;
     [SerializeField] float PosisiBawah;
 
-
-    private void FixedUpdate()
+    private void Start()
     {
-        if (Kunci)
-        {
-            PusatRotasi.rotation = Quaternion.Euler(PosisiBawah, PusatRotasi.rotation.y, PusatRotasi.rotation.z);
-        }
-        else
-        {
-            PusatRotasi.rotation = Quaternion.Euler(PosisiAtas, PusatRotasi.rotation.y, PusatRotasi.rotation.z);
-        }
+        //ParTrans = GetComponentInParent<Transform>();
+        PusatRotasi.localEulerAngles = new Vector3(PosisiAtas, 0, 0);
     }
 
-    private void OnMouseDown()
+    private void OnMouseDown() 
     {
         Debug.Log(Kunci);
         Kunci = !Kunci;
+        Swap();
     }
 
+    private void Swap()
+    {
+        if (Kunci)
+        {
+            //PusatRotasi.rotation = Quaternion.Euler(PosisiBawah, -ParTrans.transform.rotation.y, -ParTrans.transform.rotation.z);
+            PusatRotasi.localEulerAngles = new Vector3(PosisiBawah, 0, 0);
+        }
+        else
+        {
+            PusatRotasi.localEulerAngles = new Vector3(PosisiAtas, 0, 0);
+            //PusatRotasi.rotation = Quaternion.Euler(PosisiAtas, -ParTrans.transform.rotation.y, -ParTrans.transform.rotation.z);
+        }
+    }
 
 }
